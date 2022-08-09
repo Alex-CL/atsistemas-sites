e9559ff2e58d7f455b8a13a67c089c7909bd8b46
Alex-CL <alex.coll.latorre@gmail.com>
2022-08-09 13:09:30 +0200

    Basic test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import  { Home } from './features';
+import { MemoryRouter } from 'react-router-dom';
+import { Home } from './features';
 
-test('renders learn react link', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders home component', () => {
+  const { getByText } = render(
+  	<MemoryRouter>
+  		<Home />
+	</MemoryRouter>
+  );
+  const listElement = getByText(/list/i)
+  const createElement = getByText(/create/i)
+  const infoElement = getByText(/info/i)
+
+  expect(listElement).toBeInTheDocument()
+  expect(createElement).toBeInTheDocument()
+  expect(infoElement).toBeInTheDocument()
 });
