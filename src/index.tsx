a25135eb27725fc5c060f41c26269fd10d587806
Alex-CL <alex.coll.latorre@gmail.com>
2022-08-10 17:15:18 +0200

    Remove unused CSS styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { AppRouter } from './routes'
 import reportWebVitals from './reportWebVitals';
 import { Layout } from './components'
