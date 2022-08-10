220c0a8d238b807e01286b9a8c266a6d917b1a58
Alex-CL <alex.coll.latorre@gmail.com>
2022-08-10 11:17:17 +0200

    Test form component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import { Home } from './features';
+import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import { Home, Form } from './features'
+import { emptySiteDTO } from './models'
 
 test('renders home component', () => {
+	const mockFn = jest.fn()
   const { getByText } = render(
   	<MemoryRouter>
   		<Home />
@@ -12,8 +14,27 @@ test('renders home component', () => {
   const listElement = getByText(/list/i)
   const createElement = getByText(/create/i)
   const infoElement = getByText(/info/i)
-
+ 
   expect(listElement).toBeInTheDocument()
   expect(createElement).toBeInTheDocument()
   expect(infoElement).toBeInTheDocument()
 });
+
+test('renders form component', () => {
+	const { getByText } = render(
+		<MemoryRouter>
+			<Form />
+		</MemoryRouter>
+	)
+	
+	const keys = [
+		'Name',
+		'Path',
+		'Public path',
+		'Key',
+		'Description',
+		'Site'
+	]
+	
+	keys.forEach((k) => expect(getByText(k + ':')).toBeInTheDocument())
+})
